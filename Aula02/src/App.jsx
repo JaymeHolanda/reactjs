7db4a32f014639ff07f733587e502abbe495dd39
unknown <jaymeholanda.dev@gmail.com>
2024-02-28 21:38:57 -0300

    atualização

--- Aula02/src/App.jsx
+++ Aula02/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import Adição from "./components/Adição"
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+  return 
+    
 }
 
+<div> 
+  
+  <Adição num1={2} num2={3} />
+  <Subtracao num1={4} num2={2} />
+  <Multiplicacao num1={9} num2={3} />
+  <Divisao num1 ={80} num2={2} />
+ 
+
+</div>
 export default App
---
+++ Aula02/src/components/Subtracao.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const subtração = (props) => {
+  return (
+    <div>
+      O resultado de num 1 e num 2: {props.num1 - props.num2}
+    </div>
+  )
+}
+
+export default subtração
