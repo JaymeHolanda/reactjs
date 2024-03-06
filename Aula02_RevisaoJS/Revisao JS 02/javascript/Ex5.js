2e6ccc271dcc833fcf62aed0ca49eef98f115ba6
jayme <jaymeholanda.dev@gmail.com>
2024-03-06 16:18:36 -0300

    atualizando

--- Aula02_RevisaoJS/Revisao JS 02/javascript/Ex5.js
+++ Aula02_RevisaoJS/Revisao JS 02/javascript/Ex5.js
@@ -1,8 +1,8 @@
 const notas = [7, 6 , 6];
 
 console.log (`Primeira nota do aluno é: ${notas[0]}`);
-console.log (`Primeira nota do aluno é: ${notas[1]}`);
-console.log (`Primeira nota do aluno é: ${notas[2]}`);
+console.log (`Segunda nota do aluno é: ${notas[1]}`);
+console.log (`Terceira nota do aluno é: ${notas[2]}`);
 console.log (`Quantidade de notas observadas são: ${notas.length}`)
 
 // usando o método reduce para somar todas as arrays
