7710c5607c32487d3fbe7e5fe99053bafbc20c28
jayme <jaymeholanda.dev@gmail.com>
2024-05-28 15:26:37 -0300

    atualizando

--- aula_04/src/components/Exercicio1.jsx
+++ aula_04/src/components/Exercicio1.jsx
@@ -4,7 +4,7 @@ const Exercicio1 = () => {
   return (
     <div>
         <p>Teste de CSS</p>
-        <img src="https://olharanimal.org/wp-content/uploads/2017/04/GO_Goiania_grupo_miau_auau_leva_50_animais_adocao_passeio_aguas_1.jpg" alt="" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Guanabara_na_Serra_Grande.jpg/1200px-Guanabara_na_Serra_Grande.jpg" alt="Onibus Guanabara" />
     </div>
   )
 }
--- aula_04/src/components/Exercicio2.jsx
+++ aula_04/src/components/Exercicio2.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Exercicio2 = () => {
   return (
-    <div style={{color: '#ff0000', backgroundColor: '#0000ff', width: '200px', textAlign: 'center'}}>
-        Exemplo de CSS inline
+    <div style={{color: "white", backgroundColor: 'green', width: '400px' , textAlign: 'center'}}>
+        Aqui temos um exemplo de CSS inline
     </div>
   )
 }
